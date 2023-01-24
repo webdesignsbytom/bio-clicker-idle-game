9210baf5f4b95fb8cf6cce944c88db0c37ee4b35
xtombrock <xtombrock1989@gmail.com>
2023-01-24 11:12:07 +0000

    level updates

--- client/src/components/level/LevelDisplay.jsx
+++ client/src/components/level/LevelDisplay.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import './levelDisplay.css'
+import React from 'react';
+import './levelDisplay.css';
+import Logo from '../../assets/images/algae.png';
 
 function LevelDisplay() {
   return (
-    <div>LevelDisplay</div>
-  )
+    <section className='level__data__container'>
+      <div className='levelImg__container'>
+        <img src={Logo} alt='logo' />
+      </div>
+      <div className='level__data'>
+        <div className='level__name'>
+          <span>Level Name</span>
+          <h6>Yoda Planet</h6>
+        </div>
+        <div className='level__targetScore'>
+          <span>Target Score</span>
+          <h6>1,000,000</h6>
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default LevelDisplay
+export default LevelDisplay;
