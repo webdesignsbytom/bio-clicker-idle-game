b8442c8e3920bae5d318e806f0eb9ce6d9d1361f
xtombrock <xtombrock1989@gmail.com>
2023-01-24 11:22:31 +0000

    very tidy

--- client/src/components/stats/PlayerData.jsx
+++ client/src/components/stats/PlayerData.jsx
@@ -11,16 +11,18 @@ function PlayerData() {
     <section className='playerData__container'>
       <div className='player__data__container'>
         <div className='player__items__data'>
-          <h4>total items {playerCharacter.totalItemsOwned}</h4>
-          <h4>total buildings {playerCharacter.totalBuildingsOwned} </h4>
+          <h6>Total buildings: {playerCharacter.totalBuildingsOwned}</h6>
+          <h6>Buildings Array {playerCharacter.buildings}</h6>
+          <h6>Total % increase: 402%</h6>
         </div>
         <div className='player__buildings__data'>
-          <h4>Buildings Array {playerCharacter.buildings}</h4>
-          <h4>items Array {playerCharacter.items}</h4>
+          <h6>Total items: {playerCharacter.totalItemsOwned}</h6>
+          <h6>Items Array {playerCharacter.items}</h6>
+          <h6>Total % increase: 1003%</h6>
         </div>
         <div className="player__fuel__data">
-          <h4>Fuel Amount: 500 tonnes</h4>
-          <h4>Fuel production: 2t per day</h4>
+          <h6>Fuel Amount: 500 tonnes</h6>
+          <h6>Fuel production: 2t per day</h6>
         </div>
       </div>
       <LevelDisplay />
