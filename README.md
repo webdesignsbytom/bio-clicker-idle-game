47d6a3e4d3067eb49bb0116c4b4ca25786804063
xtombrock <xtombrock1989@gmail.com>
2023-01-25 13:37:06 +0000

    save

--- README.md
+++ README.md
@@ -15,10 +15,10 @@ Server database to store game data
 2. Basic stats include TotalScore, PointsPerClick(PPC), PointsPerSecond(PPS)
 3. TotalScore = PPC + PPS
 4. Other stats
-   1. Fuel
-   2. Power
-   3. CO2 saved
-   4. Cities Powered
+   1. Fuel - gets area
+   2. Power - gets area
+   3. CO2 saved - stat on power
+   4. Cities Powered - stat on power
 5. Each Unit of algae produces x amount of Fuel to be used later
 6. Levels have a total of 'Power Produced' to win
 7. Buildings and Items
@@ -26,3 +26,11 @@ Server database to store game data
    2. Increases PPC or PPS
    3. Option increase for Power stat
 8. Cities Powered Stat can be displayed using a dark area that lights up like a city as power is produced
+
+### Stats
+1. Current PPS
+2. Current PPC
+3. TotalScore
+4. Current Fuel Total
+5. FPS fuel per second
+6. 
--- client/src/components/level/LevelDisplay.jsx
+++ client/src/components/level/LevelDisplay.jsx
@@ -5,9 +5,28 @@ import Logo from '../../assets/images/algae.png';
 function LevelDisplay() {
   return (
     <section className='level__data__container'>
-      <div className='levelImg__container'>
-        <img src={Logo} alt='logo' />
+      <div className="fuel__data__container">
+        <div className="fuel__production">
+          <span>Fuel Production</span>
+          <h6>10,000 gallons</h6>
+        </div>
+        <div className="total__fuel">
+          <span>Total Fuel</span>
+          <h6>1 million gallons</h6>
+        </div>
       </div>
+
+      <div className="power__data__container">
+        <div className="power__production">
+          <span>Power Production</span>
+          <h6>10 TW</h6>
+        </div>
+        <div className="power__coverage">
+          <span>Power Supply</span>
+          <h6>50 Cities</h6>
+        </div>
+      </div>
+
       <div className='level__data'>
         <div className='level__name'>
           <span>Level Name</span>
