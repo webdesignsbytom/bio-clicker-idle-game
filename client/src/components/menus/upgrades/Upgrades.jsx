37357ad0c069f96abf2c3be0f9eab99801cca287
Thomas Brockington <xtombrock1989@gmail.com>
2023-02-05 04:56:31 +0000

    save

--- client/src/components/menus/upgrades/Upgrades.jsx
+++ client/src/components/menus/upgrades/Upgrades.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react';
+import UpgradeDB from '../../../db/upgrades.json';
+
+import { GameContext } from '../../../context/GameContext';
+import { useContext } from 'react';
 
 function Upgrades() {
+  const { playerCharacter, setPlayerCharacter } = useContext(GameContext);
+  const [itemsArray, setItemsArray] = useState(UpgradeDB);
+
   return (
-    <div>Upgrades</div>
-  )
+    <>
+      <div>Upgrades</div>
+    </>
+  );
 }
 
-export default Upgrades
+export default Upgrades;
